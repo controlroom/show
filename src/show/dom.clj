e478f262a89d72f7fc8138ce00dc21fdfc69e042
rabidpraxis <kevin@kevinwebster.me>
2014-10-08 14:11:23 -0700

    removed var from dom fns

--- src/show/dom.clj
+++ src/show/dom.clj
@@ -9,7 +9,7 @@
     main map mark menu menuitem meta meter nav noscript object ol optgroup
     option output p param pre progress q rp rt ruby s samp script section
     select small source span strong style sub summary sup table tbody td
-    textarea tfoot th thead time title tr track u ul var video wbr
+    textarea tfoot th thead time title tr track u ul video wbr
 
     ;; svg
     circle g line path polyline rect svg text defs linearGradient polygon
