1504aa4b25b5fd3dd6ae704ff32286d7c334d567
Kevin Webster <kwebster@renewfinancial.com>
2018-06-21 08:38:58 -0700

    better show.dom coverage

--- test/show/dom_test.clj
+++ test/show/dom_test.clj
@@ -6,6 +6,10 @@
 (defn args-eql [process-args-args expected]
   (is (= (apply dom/process-args process-args-args) expected)))
 
+(defn test-convert-to-js [args]
+  (with-meta args {:js true}))
+(intern 'show.dom 'convert-to-js test-convert-to-js)
+
 (deftest process-args-test
   (testing "no options in arglist"
     (args-eql '("Foo")
@@ -13,6 +17,9 @@
   (testing "nil options"
     (args-eql '(nil nil)
               [{} nil]))
+  (testing "calls 'convert-to-js on nested hashes"
+    (let [[{:keys [nested-hash]} _] (dom/process-args {:nested-hash {:b "string"}})]
+      (is (= (meta nested-hash) {:js true}))))
   (testing 'preproces-opts
     (testing "rewriting :class to :className"
       (args-eql '({:class "bar"})
@@ -28,6 +35,9 @@
 (intern 'show.dom 'element-creator test-creator)
 
 (deftest element-test
+  (testing "calls 'convert-to-js on opts & body"
+    (let [[_ arg1 arg2] (dom/element :noop {:foo :bar} "string")]
+      (every? #(true? (:js (meta %))) [arg1 arg2])) )
   (testing "Delegates to element-creator"
     (is (= (dom/element :h2 {:className "foo"} "Bar")
            ["h2" {:className "foo"} '("Bar")]))))
